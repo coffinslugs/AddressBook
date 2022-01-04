fb90ee565ba91d1c5ccc615407fcea44c1e2d447
jcodesthings <55113474+jcodesthings@users.noreply.github.com>
2022-01-04 14:12:49 -0700

    contacts table
    properties set and contacts table created.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AddressBook.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,6 @@ namespace AddressBook.Data
 
         }
 
+        public DbSet<Contact> Contacts { get; set; }
     }
 }
