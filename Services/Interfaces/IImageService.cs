32b850877897b3d28af9847206d71d1070ad01f3
jcodesthings <55113474+jcodesthings@users.noreply.github.com>
2022-01-05 20:11:34 -0700

    Create Image Interface

---
+++ Services/Interfaces/IImageService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace AddressBook.Services.Interfaces
+{
+    interface IImageService
+    {
+        public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+        public string ConvertByteArrayToFile(byte[] filedata, string extension);
+
+    }
+}
